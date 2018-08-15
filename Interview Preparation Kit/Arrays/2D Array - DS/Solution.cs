e9e69889eb5966470c2bbc43fe5d6ae7716f3a32
Bruno de Andrade <bruno.andrade23@gmail.com>
2018-08-14 23:09:57 -0300

    Created the project and added two solutions

---
+++ Interview Preparation Kit/Arrays/2D Array - DS/Solution.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Interview_Preparation_Kit.Arrays._2D_Array___DS
+{
+    internal class Solution
+    {
+        private static int hourglassSum(int[][] arr)
+        {
+            int? maxSum = null;
+
+            for (int x = 0; x < 4; x++)
+            {
+                for (int y = 0; y < 4; y++)
+                {
+                    int currentSum = arr[y][x] + arr[y][x + 1] + arr[y][x + 2] +
+                                     arr[y + 1][x + 1] +
+                                     arr[y + 2][x] + arr[y + 2][x + 1] + arr[y + 2][x + 2];
+
+                    if (maxSum == null || currentSum > maxSum)
+                    {
+                        maxSum = currentSum;
+                    }
+                }
+            }
+
+            return maxSum.Value;
+        }
+
+        private static void Main(string[] args)
+        {
+            int[][] arr = new int[6][];
+
+            for (int i = 0; i < 6; i++)
+            {
+                arr[i] = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            }
+
+            var watch = Stopwatch.StartNew();
+
+            int result = hourglassSum(arr);
+
+            watch.Stop();
+
+            Console.WriteLine(result);
+
+            Console.WriteLine(string.Format("Elapsed time: {0} seconds", watch.Elapsed.TotalSeconds));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Interview Preparation Kit/Arrays/Arrays Left Rotation/Solution.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Interview_Preparation_Kit.Arrays.Arrays_Left_Rotation
+{
+    internal class Solution
+    {
+        private static int[] rotLeft(int[] a, int d)
+        {
+            for (int rotationIndex = 0; rotationIndex < d; rotationIndex++)
+            {
+                int firstValue = a[0];
+
+                Array.Copy(a, 1, a, 0, a.Length - 1);
+
+                a[a.Length - 1] = firstValue;
+            }
+
+            return a;
+        }
+
+        private static void Main(string[] args)
+        {
+            string[] nd = Console.ReadLine().Split(' ');
+
+            int n = Convert.ToInt32(nd[0]);
+
+            int d = Convert.ToInt32(nd[1]);
+
+            int[] a = Array.ConvertAll(Console.ReadLine().Split(' '), aTemp => Convert.ToInt32(aTemp));
+
+            var watch = Stopwatch.StartNew();
+
+            int[] result = rotLeft(a, d);
+
+            watch.Stop();
+
+            Console.WriteLine(string.Join(" ", result));
+
+            Console.WriteLine(string.Format("Elapsed time: {0} seconds", watch.Elapsed.TotalSeconds));
+            Console.ReadKey();
+        }
+    }
+}

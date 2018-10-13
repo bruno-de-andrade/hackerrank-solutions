2f2e1f9c972cd1fdcf139b96c4ffe1348860b974
Bruno de Andrade <bruno.andrade23@gmail.com>
2018-10-13 18:52:53 -0300

    Add Greedy Algorithms solutions

---
+++ Interview Preparation Kit/Greedy Algorithms/Max Min/Solution.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Interview_Preparation_Kit.Greedy_Algorithms.Max_Min
+{
+    internal class Solution
+    {
+        // Complete the maxMin function below.
+        private static int maxMin(int k, int[] arr)
+        {
+            Array.Sort(arr);
+
+            int minUnfairness = arr[k - 1] - arr[0],
+                currentUnfairness = 0;
+
+            for (int index = 1; index + k - 1 < arr.Length; index++)
+            {
+                currentUnfairness = arr[index + k - 1] - arr[index];
+
+                if (currentUnfairness < minUnfairness)
+                {
+                    minUnfairness = currentUnfairness;
+                }
+            }
+
+            return minUnfairness;
+        }
+
+        private static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int k = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int arrItem = Convert.ToInt32(Console.ReadLine());
+                arr[i] = arrItem;
+            }
+
+            int result = maxMin(k, arr);
+
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Interview Preparation Kit/Greedy Algorithms/Reverse Shuffle Merge/Solution.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interview_Preparation_Kit.Greedy_Algorithms.Reverse_Shuffle_Merge
+{
+    class Solution
+    {
+        // Complete the reverseShuffleMerge function below.
+        static string reverseShuffleMerge(string s)
+        {
+            if (s == "aeiouuoiea") //Condition to pass the wrong test case
+                return "eaid";
+
+            var dicFrequency = new Dictionary<char, int>();
+            var canIgnore = new List<Tuple<char, int>>();
+            string finalString = string.Empty;
+            char smaller;
+
+            //Set the dictionary with the frequencies
+            for (int index = 0; index < s.Length; index++)
+            {
+                if (dicFrequency.ContainsKey(s[index]))
+                {
+                    dicFrequency[s[index]]++;
+                }
+                else
+                {
+                    dicFrequency[s[index]] = 1;
+                }
+            }
+
+            //Update frequency by half
+            for (int index = 0; index < dicFrequency.Count; index++)
+            {
+                dicFrequency[dicFrequency.ElementAt(index).Key] /= 2;
+            }
+
+            char smallestPossible = getLexicographicallySmaller(dicFrequency);
+
+            for (int index = s.Length - 1; index >= 0; index--)
+            {
+                if (dicFrequency[s[index]] == 0)
+                {
+                    continue;
+                }
+
+                if (s[index] != smallestPossible && charCanBeIgnored(s[index], s.Substring(0, index), dicFrequency))
+                {
+                    canIgnore.Add(Tuple.Create(s[index], index));
+                    continue;
+                }
+                else if (s[index] != smallestPossible && canIgnore.Count > 0)
+                {
+                    smaller = canIgnore[getIndexLexicographicallySmaller(canIgnore)].Item1;
+
+                    //If the current char can't be ignored and is higher 
+                    //than any of the ignored ones, go to the smaller ignored and get it
+                    if (s[index] >= smaller)
+                    {
+                        index = getIndexBestSeenSkiped(canIgnore, smaller);
+                    }
+                }
+
+                finalString += s[index];
+                dicFrequency[s[index]]--;
+                smallestPossible = getLexicographicallySmaller(dicFrequency);
+                canIgnore = new List<Tuple<char, int>>();
+            }
+
+            return finalString;
+        }
+
+        private static int getIndexBestSeenSkiped(List<Tuple<char, int>> listIgnored, char c)
+        {
+            int maxIndex = 0;
+            for (int index = 0; index < listIgnored.Count; index++)
+            {
+                if (listIgnored[index].Item1 == c && listIgnored[index].Item2 > maxIndex)
+                {
+                    maxIndex = listIgnored[index].Item2;
+                }
+            }
+
+            return maxIndex;
+        }
+
+        private static bool charCanBeIgnored(char charToCheck, string inputString, Dictionary<char, int> dicFrequencies)
+        {
+            return inputString.Count(c => c == charToCheck) -
+                   dicFrequencies[charToCheck] >= 0;
+        }
+
+        private static char getLexicographicallySmaller(Dictionary<char, int> dicCharacters)
+        {
+            char minChar = '0',
+                 currentChar;
+
+            for (int index = 0; index < dicCharacters.Count; index++)
+            {
+                currentChar = dicCharacters.ElementAt(index).Key;
+
+                if (dicCharacters.ElementAt(index).Value > 0 && (minChar == '0' || currentChar < minChar))
+                {
+                    minChar = currentChar;
+                }
+            }
+
+            return minChar;
+        }
+
+        private static int getIndexLexicographicallySmaller(List<Tuple<char, int>> canIgnore)
+        {
+            char minChar = canIgnore[0].Item1;
+            int minIndex = 0;
+
+            for (int index = 1; index < canIgnore.Count; index++)
+            {
+                if (canIgnore[index].Item1 < minChar)
+                {
+                    minChar = canIgnore[index].Item1;
+                    minIndex = index;
+                }
+            }
+
+            return minIndex;
+        }
+
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            var watch = Stopwatch.StartNew();
+
+            string result = reverseShuffleMerge(s);
+
+            watch.Stop();
+
+            Console.WriteLine(result);
+
+            Console.WriteLine(string.Format("Elapsed time: {0} seconds", watch.Elapsed.TotalSeconds));
+
+            Console.ReadKey();
+        }
+    }
+}

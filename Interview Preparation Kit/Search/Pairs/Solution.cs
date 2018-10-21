50bd16f611b80f184beed12d2c3de7cb9475e842
Bruno de Andrade <bruno.andrade23@gmail.com>
2018-10-21 01:12:54 -0300

    Add Search problem solution

---
+++ Interview Preparation Kit/Search/Pairs/Solution.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Interview_Preparation_Kit.Search.Pairs
+{
+    internal class Solution
+    {
+        // Complete the pairs function below.
+        private static int pairs(int k, int[] arr)
+        {
+            int pairsCount = 0,
+                difference = 0;
+
+            Array.Sort(arr);
+
+            for (int i = arr.Length - 1; i > 0; i--)
+            {
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    difference = arr[i] - arr[j];
+
+                    if (difference == k)
+                    {
+                        pairsCount++;
+                    }
+                    else if (difference > k)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            return pairsCount;
+        }
+
+        private static void Main(string[] args)
+        {
+            string[] nk = Console.ReadLine().Split(' ');
+
+            int n = Convert.ToInt32(nk[0]);
+
+            int k = Convert.ToInt32(nk[1]);
+
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            int result = pairs(k, arr);
+
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}

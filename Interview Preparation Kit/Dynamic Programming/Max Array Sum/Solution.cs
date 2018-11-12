6069117d3804ed8509f24c2991fd1f1b734ad52b
Bruno de Andrade <bruno.andrade23@gmail.com>
2018-11-11 23:43:49 -0200

    Add solution to a dynamic programming problem

---
+++ Interview Preparation Kit/Dynamic Programming/Max Array Sum/Solution.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Interview_Preparation_Kit.Dynamic_Programming.Max_Array_Sum
+{
+    internal class Solution
+    {
+        // Complete the maxSubsetSum function below.
+        private static int maxSubsetSum(int[] arr)
+        {
+            int maxSum = Math.Max(arr[0], arr[1]);
+            int currentMax = -1;
+
+            arr[1] = Math.Max(arr[0], arr[1]);
+
+            for (int i = 2; i < arr.Length; i++)
+            {
+                currentMax = Math.Max(arr[i], arr[i] + arr[i - 2]);
+                maxSum = Math.Max(maxSum, currentMax);
+
+                arr[i] = maxSum;
+            }
+
+            return maxSum;
+        }
+
+        private static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            int res = maxSubsetSum(arr);
+
+            Console.WriteLine(res);
+
+            Console.ReadKey();
+        }
+    }
+}

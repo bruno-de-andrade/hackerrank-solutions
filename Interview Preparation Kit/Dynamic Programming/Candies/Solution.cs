da270855807bc5c59885e9e41af5b7ffdab714fa
Bruno de Andrade <bruno.andrade23@gmail.com>
2018-11-12 23:26:19 -0200

    Add candies(dynamic programming) problem solution

---
+++ Interview Preparation Kit/Dynamic Programming/Candies/Solution.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Interview_Preparation_Kit.Dynamic_Programming.Candies
+{
+    internal class Solution
+    {
+        // Complete the candies function below.
+        private static long candies(int n, int[] arr)
+        {
+            long[] arrAmounts = new long[arr.Length];
+            long totalAmount = 0;
+
+            arrAmounts[0] = 1;
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                arrAmounts[i] = arr[i] > arr[i - 1] ? arrAmounts[i - 1] + 1 : 1;
+            }
+
+            totalAmount += arrAmounts[arr.Length - 1];
+
+            for (int i = arr.Length - 2; i >= 0; i--)
+            {
+                if (arr[i] > arr[i + 1] && arrAmounts[i] <= arrAmounts[i + 1])
+                {
+                    arrAmounts[i] = arrAmounts[i + 1] + 1;
+                }
+
+                totalAmount += arrAmounts[i];
+            }
+
+            return totalAmount;
+        }
+
+        private static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int arrItem = Convert.ToInt32(Console.ReadLine());
+                arr[i] = arrItem;
+            }
+
+            long result = candies(n, arr);
+
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}

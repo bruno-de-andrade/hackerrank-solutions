f820739af6ec406d01e3f8015561e75d4fb0176b
Bruno de Andrade <bruno.andrade23@gmail.com>
2018-11-15 18:36:22 -0200

    Add search problem solution

---
+++ Interview Preparation Kit/Miscellaneous/Flipping bits/Solution.js
@@ -0,0 +1,55 @@
+﻿'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the flippingBits function below.
+function flippingBits(N) {
+    let binary = N.toString(2),
+        invertedBinary = '';
+    
+    for (var i = 0; i < binary.length; i++) {
+        invertedBinary += binary[i] === "0" ? "1" : "0";
+    }
+    
+    invertedBinary = invertedBinary.padStart(32, '1');
+
+    return parseInt(invertedBinary, 2);
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const q = parseInt(readLine(), 10);
+
+    for (let qItr = 0; qItr < q; qItr++) {
+        const n = parseInt(readLine(), 10);
+
+        const result = flippingBits(n);
+
+        ws.write(result + '\n');
+    }
+
+    ws.end();
+}
---
+++ Interview Preparation Kit/Search/Triple Sum/Solution.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Interview_Preparation_Kit.Search.Triple_Sum
+{
+    internal class Solution
+    {
+        // Complete the triplets function below.
+        private static long triplets(int[] a, int[] b, int[] c)
+        {
+            Array.Sort(a);
+            Array.Sort(b);
+            Array.Sort(c);
+
+            a = a.Distinct().ToArray();
+            b = b.Distinct().ToArray();
+            c = c.Distinct().ToArray();
+
+            int lastIndexA = a.Length - 1, lastIndexC = c.Length - 1;
+            long total = 0;
+
+            for (int indexB = b.Length - 1; indexB >= 0; indexB--)
+            {
+                for (int indexA = lastIndexA; indexA >= 0; indexA--)
+                {
+                    if (a[indexA] <= b[indexB])
+                    {
+                        lastIndexA = indexA;
+                        break;
+                    }
+                }
+
+                for (int indexC = lastIndexC; indexC >= 0; indexC--)
+                {
+                    if (c[indexC] <= b[indexB])
+                    {
+                        lastIndexC = indexC;
+                        total += (long)(lastIndexA + 1) * (lastIndexC + 1);
+                        break;
+                    }
+                }
+            }
+
+            return total;
+        }
+
+        private static void Main(string[] args)
+        {
+            string[] lenaLenbLenc = Console.ReadLine().Split(' ');
+
+            int lena = Convert.ToInt32(lenaLenbLenc[0]);
+
+            int lenb = Convert.ToInt32(lenaLenbLenc[1]);
+
+            int lenc = Convert.ToInt32(lenaLenbLenc[2]);
+
+            int[] arra = Array.ConvertAll(Console.ReadLine().Split(' '), arraTemp => Convert.ToInt32(arraTemp));
+
+            int[] arrb = Array.ConvertAll(Console.ReadLine().Split(' '), arrbTemp => Convert.ToInt32(arrbTemp));
+
+            int[] arrc = Array.ConvertAll(Console.ReadLine().Split(' '), arrcTemp => Convert.ToInt32(arrcTemp));
+
+            var watch = Stopwatch.StartNew();
+
+            long ans = triplets(arra, arrb, arrc);
+
+            watch.Stop();
+
+            Console.WriteLine(ans);
+
+            Console.WriteLine(string.Format("Elapsed time: {0} seconds", watch.Elapsed.TotalSeconds));
+
+            Console.ReadKey();
+        }
+    }
+}

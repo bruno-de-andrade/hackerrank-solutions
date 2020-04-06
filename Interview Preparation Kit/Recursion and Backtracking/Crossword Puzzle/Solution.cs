71bd4c29688e1b37fa55c12811fa00eca1cc6b67
Bruno de Andrade <bruno.andrade23@gmail.com>
2020-04-06 20:30:12 +0200

    Add Recursion and Backtracking solutions

---
+++ Interview Preparation Kit/Recursion and Backtracking/Crossword Puzzle/Solution.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Interview_Preparation_Kit.Recursion_and_Backtracking.Crossword_Puzzle
+{
+    class Solution
+    {
+        static string[] crosswordPuzzle(string[] crossword, string words)
+        {
+            if (string.IsNullOrEmpty(words))
+            {
+                return crossword;
+            }
+
+            string[] crosswordAttempt = new string[crossword.Length];
+            crossword.CopyTo(crosswordAttempt, 0);
+
+            var wordsArray = words.Split(';').ToList();
+
+            for (int row = 0; row < 10; row++)
+            {
+                int wordSpaceIndex = crosswordAttempt[row].IndexOf('-');
+
+                if (wordSpaceIndex != -1)
+                {
+                    // if next horizontal space is also a '-', its a horizontal word
+                    bool spaceIsHorizontal = wordSpaceIndex < 9 && crosswordAttempt[row][wordSpaceIndex + 1] == '-';
+                    int spaceLength = spaceIsHorizontal
+                        ? getSpaceLengthHorizontal(crossword[row], wordSpaceIndex)
+                        : getSpaceLengthVertical(crossword, row, wordSpaceIndex);
+
+                    // Try fill available words
+                    for (int i = 0; i < wordsArray.Count; i++)
+                    {
+                        string startLetter = getSpaceFilledStartLetter(crosswordAttempt, spaceIsHorizontal, wordSpaceIndex, row);
+
+                        var fullWord = wordsArray[i];
+                        var word = wordsArray[i].Substring(startLetter.Length, wordsArray[i].Length - startLetter.Length);
+
+                        // If word isn't the same size as the space or word doesn't start with the letter pre-filled in the space
+                        if (word.Length != spaceLength || (startLetter.Length > 0 && startLetter != fullWord[0].ToString()))
+                            continue;
+
+                        if (spaceIsHorizontal)
+                        {
+                            // Fill word horizontally
+                            crosswordAttempt[row] = crosswordAttempt[row].Replace(crosswordAttempt[row].Substring(wordSpaceIndex, word.Length), word);
+                        }
+                        else
+                        {
+                            // Fill word vertically
+                            for (int j = 0; j < spaceLength; j++)
+                            {
+                                crosswordAttempt[row + j] = 
+                                    crosswordAttempt[row + j].Substring(0, wordSpaceIndex) + 
+                                    word[j] + 
+                                    crosswordAttempt[row + j].Substring(wordSpaceIndex + 1, 10 - wordSpaceIndex - 1);
+                            }
+                        }
+
+                        var newWordsArray = new List<string>(wordsArray);
+                        newWordsArray.RemoveAt(i);
+
+                        var finalCrossword = crosswordPuzzle(crosswordAttempt, string.Join(";", newWordsArray));
+
+                        // If all positions are filled
+                        if (!finalCrossword.ToList().Any(a => a.Contains("-")))
+                        {
+                            return finalCrossword;
+                        }
+                        else
+                        {
+                            // Thats not the correct word for this position, remove from the crossword
+                            crossword.CopyTo(crosswordAttempt, 0);
+                        }
+                    }
+
+                    // No word fits in the space, returning;
+                    return crosswordAttempt;
+                }
+            }
+
+            return crossword;
+        }
+
+        static int getSpaceLengthHorizontal(string row, int columnIndex)
+        {
+            var startIndex = columnIndex;
+
+            while (columnIndex < 10 && row[columnIndex] != '+' && row[columnIndex] != 'X')
+            {
+                columnIndex++;
+            }
+
+            return columnIndex - startIndex;
+        }
+
+        static int getSpaceLengthVertical(string[] crossword, int rowIndex, int columnIndex)
+        {
+            var index = rowIndex;
+
+            while (index < 10 && crossword[index][columnIndex] != '+' && crossword[index][columnIndex] != 'X')
+            {
+                index++;
+            }
+
+            return index - rowIndex;
+        }
+
+        // Check is space was already partially filled
+        static string getSpaceFilledStartLetter(string[] crossword, bool spaceIsHorizontal, int wordSpaceIndex, int row)
+        {
+            string startLetter = string.Empty;
+
+            // Check is space was already partially filled
+            if (spaceIsHorizontal && wordSpaceIndex > 0 && crossword[row][wordSpaceIndex - 1] != '+' && crossword[row][wordSpaceIndex - 1] != 'X')
+            {
+                startLetter = crossword[row][wordSpaceIndex - 1].ToString();
+            }
+            else if (!spaceIsHorizontal && row > 0 && crossword[row - 1][wordSpaceIndex] != '+' && crossword[row - 1][wordSpaceIndex] != 'X')
+            {
+                startLetter = crossword[row - 1][wordSpaceIndex].ToString();
+            }
+
+            return startLetter;
+        }
+
+        static void Main(string[] args)
+        {
+            string[] crossword = new string[10];
+
+            var file = new System.IO.StreamReader(@"Recursion and Backtracking\Crossword Puzzle\testCase1.txt");
+
+            for (int i = 0; i < 10; i++)
+            {
+                string crosswordItem = file.ReadLine();
+                crossword[i] = crosswordItem;
+            }
+
+            file.Close();
+
+            string words = "CALIFORNIA;NIGERIA;CANADA;TELAVIV";
+
+            string[] result = crosswordPuzzle(crossword, words);
+
+            Console.WriteLine(string.Join("\n", result));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Interview Preparation Kit/Recursion and Backtracking/Recursion Davis Staircase/Solution.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Interview_Preparation_Kit.Recursion_and_Backtracking.Recursion_Davis_Staircase
+{
+    class Solution
+    {
+        static Dictionary<int, int> dic = new Dictionary<int, int>();
+
+        static int stepPerms(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+
+            int count = 0;
+
+            // Uses memoization to store sub-problem solutions
+            if (!dic.ContainsKey(n))
+            {
+                for (int steps = 3; steps > 0; steps--)
+                {
+                    if (n >= steps)
+                    {
+                        count += stepPerms(n - steps);
+                    }
+                }
+
+                dic.Add(n, count);
+            }
+
+            return dic[n];
+        }
+
+        static void Main(string[] args)
+        {
+            var file = new System.IO.StreamReader(@"Recursion and Backtracking\Recursion Davis Staircase\testCase1.txt");
+
+            int s = Convert.ToInt32(file.ReadLine());
+
+            var watch = Stopwatch.StartNew();
+
+            for (int sItr = 0; sItr < s; sItr++)
+            {
+                int n = Convert.ToInt32(file.ReadLine());
+
+                int res = stepPerms(n);
+
+                Console.WriteLine(res);
+            }
+
+            Console.WriteLine(string.Format("Elapsed time: {0} seconds", watch.Elapsed.TotalSeconds));
+
+            file.Close();
+
+            Console.ReadKey();
+        }
+    }
+}
